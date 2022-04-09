d73eea7bf3b53022976e6dda9009bdbf78a0a796
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-09 06:56:44 +0530

    Update roboshop-shared-library/

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -2,7 +2,7 @@ def lintChecks() {
   sh '''
     # We commented this because devs gonna check the failures.
     #~/node_modules/jslint/bin/jslint.js server.js
-    echo Link Check 
+    echo Link Check for ${COMPONENT}
   '''
 }
 
