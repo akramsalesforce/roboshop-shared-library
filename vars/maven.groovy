202aad6df438730a33f2cfff6e906e4d72c17294
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-11 07:09:35 +0530

    Update roboshop-shared-library/

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -29,6 +29,8 @@ def call() {
       stage('SonarCheck') {
         steps {
           script {
+            sh 'mvn clean compile'
+            env.ARGS="-Dsonar.java.binaries=target/"
             common.sonarCheck()
           }
         }
