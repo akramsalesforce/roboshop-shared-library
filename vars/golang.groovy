b01103738f7ef86197972b5048529671e02184a1
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-11 07:15:16 +0530

    Update roboshop-shared-library/

---
+++ vars/golang.groovy
@@ -0,0 +1,43 @@
+def lintChecks() {
+  sh '''
+    # We commented this because devs gonna check the failures.
+    #~/node_modules/jslint/bin/jslint.js server.js
+    echo Link Check for ${COMPONENT}
+  '''
+}
+
+def call() {
+  pipeline {
+    agent any
+
+    environment {
+      SONAR = credentials('SONAR')
+    }
+
+    stages {
+
+      // For Each Commit
+      stage('Lint Checks') {
+        steps {
+          script {
+            lintChecks()
+          }
+        }
+      }
+
+      stage('SonarCheck') {
+        steps {
+          script {
+            env.ARGS="-Dsonar.sources=."
+            common.sonarCheck()
+          }
+        }
+      }
+
+
+    } // End of Stages
+
+  }
+
+
+}
