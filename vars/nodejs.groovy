c555e99e331b0091751f82b6aa26c31cdcadee07
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-13 06:25:19 +0530

    Update roboshop-shared-library/

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -67,7 +67,7 @@ def call() {
         }
         steps {
           script {
-            def UPLOAD_STATUS=sh(returnStdout: true, script: 'curl -v -s http://172.31.0.90:8081/service/rest/repository/browse/${COMPONENT}')
+            def UPLOAD_STATUS=sh(returnStdout: true, script: 'curl -L -v -s http://172.31.0.90:8081/service/rest/repository/browse/${COMPONENT}')
             print UPLOAD_STATUS
           }
         }
