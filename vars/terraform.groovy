b6cf9d0cce4d6dc28a365b58b555c1013cb4ab03
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-22 06:31:46 +0530

    Update roboshop-shared-library/

---
+++ vars/terraform.groovy
@@ -0,0 +1,19 @@
+def call() {
+
+  properties([
+      parameters([
+          choice(choices: 'dev\nprod', description: "Choose Environement", name: "ENV"),
+      ]),
+  ])
+
+  node {
+
+    sh 'rm -rf *'
+    git branch: 'main', url: "https://github.com/raghudevopsb63/${REPONAME}"
+
+    stage('Terrafile Init') {
+      sh 'terrafile -f env-${ENV}/Terrafile'
+    }
+  }
+}
+
