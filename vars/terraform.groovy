8cf63647b1fdaab8308f715370e994f8a8d110d3
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-04-22 06:34:14 +0530

    Update roboshop-shared-library/

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -11,9 +11,18 @@ def call() {
     sh 'rm -rf *'
     git branch: 'main', url: "https://github.com/raghudevopsb63/${REPONAME}"
 
-    stage('Terrafile Init') {
+    stage('Terrafile INIT') {
       sh 'terrafile -f env-${ENV}/Terrafile'
     }
+
+    stage('Terraform INIT') {
+      sh 'terraform init -backend-config=env-${ENV}/${ENV}-backend.tfvars'
+    }
+
+    stage('Terraform Plan') {
+      sh 'terraform plan -var-file=env-${ENV}/${ENV}.tfvars'
+    }
+
   }
 }
 
