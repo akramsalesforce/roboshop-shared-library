c9cba99a0ca164636c67077faf18e1f336c17113
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-24 07:18:35 +0530

    Update roboshop-shared-library/

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -10,9 +10,9 @@ def call() {
     if (env.TAG_NAME != null) {
       stage('Docker Build') {
         sh """
-          docker tag 633788536644.dkr.ecr.us-east-1.amazonaws.com/frontend:latest 633788536644.dkr.ecr.us-east-1.amazonaws.com/frontend:${TAG_NAME}
+          docker tag 633788536644.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:latest 633788536644.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME}
           aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 633788536644.dkr.ecr.us-east-1.amazonaws.com
-          docker push 633788536644.dkr.ecr.us-east-1.amazonaws.com/frontend:${TAG_NAME}
+          docker push 633788536644.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:${TAG_NAME}
         """
       }
     }
