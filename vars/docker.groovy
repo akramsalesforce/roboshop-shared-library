031779d94f9ce061b55e06bd85b86bf5dec0f1a2
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-24 07:10:10 +0530

    Update roboshop-shared-library/

--- vars/docker.groovy
+++ vars/docker.groovy
@@ -4,7 +4,17 @@ def call() {
     git branch: 'main', url: "https://github.com/raghudevopsb63/${COMPONENT}"
 
     stage('Docker Build') {
-      sh "docker build ."
+      sh "docker build -t 633788536644.dkr.ecr.us-east-1.amazonaws.com/${COMPONENT}:latest ."
+    }
+
+    if (env.TAG_NAME != null) {
+      stage('Docker Build') {
+        sh """
+          docker tag 633788536644.dkr.ecr.us-east-1.amazonaws.com/frontend:latest 633788536644.dkr.ecr.us-east-1.amazonaws.com/frontend:${TAG_NAME}
+          aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 633788536644.dkr.ecr.us-east-1.amazonaws.com
+          docker push 633788536644.dkr.ecr.us-east-1.amazonaws.com/frontend:${TAG_NAME}
+        """
+      }
     }
 
   }
