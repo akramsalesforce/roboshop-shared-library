9ebd24690da57e24f317e6797513bff21e76e50a
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-05-24 06:40:58 +0530

    Update roboshop-shared-library/

---
+++ vars/docker.groovy
@@ -0,0 +1,10 @@
+def call() {
+  node {
+    sh 'rm -rf *'
+    git branch: 'main', url: "https://github.com/raghudevopsb63/${COMPONENT}"
+
+    stage('Docker Build') {
+      sh "docker build ."
+    }
+  }
+}
