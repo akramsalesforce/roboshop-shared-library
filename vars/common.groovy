b7a00baf474643877006b10511f5aee228bf0d7a
Akram, khan <Akram.khan200@gmail.com>
2024-01-08 23:39:52 +0530

    changes

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,8 +1,8 @@
 def sonarCheck() {
     stage('Sonar Code Analysis') {
         sh '''
-      sonar-scanner -Dsonar.host.url=http://172.31.43.143:9000 -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} ${ARGS}
-      sonar-quality-gate.sh ${SONAR_USR} ${SONAR_PSW} 172.31.43.143 ${COMPONENT}
+      sonar-scanner -Dsonar.host.url=http://http://35.173.217.40/:9000 -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW} ${ARGS}
+      sonar-quality-gate.sh ${SONAR_USR} ${SONAR_PSW} http://35.173.217.40 ${COMPONENT}
       echo Sonar Checks for ${COMPONENT}
     '''
     }
@@ -14,15 +14,15 @@ def lintChecks() {
         if (env.APP_TYPE == "nodejs") {
             sh '''
         # We commented this because devs gonna check the failures.
-        #~/node_modules/jslint/bin/jslint.js server.js
+        ~/node_modules/jslint/bin/jslint.js server.js
         echo Link Check for ${COMPONENT}
       '''
         }
         else if (env.APP_TYPE == "maven") {
             sh '''
       # We commented this because devs gonna check the failures.
-      #~/node_modules/jslint/bin/jslint.js server.js
-      #mvn checkstyle:check
+      ~/node_modules/jslint/bin/jslint.js server.js
+      mvn checkstyle:check
       echo Lint Check for ${COMPONENT}
     '''
         }
