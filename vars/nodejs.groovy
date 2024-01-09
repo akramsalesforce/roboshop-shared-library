d5ad4f0770b15c9361e550b7f2c76bb01b750550
Akram, khan <Akram.khan200@gmail.com>
2024-01-09 19:14:30 +0530

    changes

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,9 +1,7 @@
 def call() {
   node {
     sh 'rm -rf *'
-    git branch: 'main', url: "https://github.com/akramsaleforce/${COMPONENT}.git"
-
-
+    git branch: 'main', url: "https://github.com/salesforce/${COMPONENT}"
     env.APP_TYPE = "nodejs"
     common.lintChecks()
     env.ARGS="-Dsonar.sources=."
